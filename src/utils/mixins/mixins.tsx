ee2a2b2e7c383bc1f044c94254b827718025a424
changhwan_park <ch.park@kaist.ac.kr>
2022-07-09 22:43:03 +0900

    chore: add button style

--- src/utils/mixins/mixins.tsx
+++ src/utils/mixins/mixins.tsx
@@ -1,6 +1,9 @@
 import { css } from 'styled-components';
 
 export const CButton = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   padding: 0;
   border: none;
   background: none;
