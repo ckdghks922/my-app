026c31f9c4bb5b3b234891e75f4e1a862243187a
changhwan_park <ch.park@kaist.ac.kr>
2022-07-18 22:34:12 +0900

    feat: canvas 사각형 그리기

--- src/components/Canvas/CanvasContainer.tsx
+++ src/components/Canvas/CanvasContainer.tsx
@@ -1,5 +1,18 @@
-import { CanvasView } from './CanvasView';
+import { useEffect, useRef } from 'react';
+import * as S from './styled';
 
 export const CanvasContainer: React.FC = () => {
-  return <CanvasView />;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext('2d');
+    ctx?.fillRect(50, 50, 50, 50);
+  }, []);
+
+  return (
+    <S.Wrapper>
+      <S.Canvas ref={canvasRef} />
+    </S.Wrapper>
+  );
 };
--- src/components/Canvas/styled.tsx
+++ src/components/Canvas/styled.tsx
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.canvas``;
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Canvas = styled.canvas`
+  width: 1000px;
+  height: 500px;
+  border: 1px solid black;
+`;
