38938e77b2cd4ba26f630ad5510cd9134ea94589
changhwan_park <ch.park@kaist.ac.kr>
2022-10-03 19:58:48 +0900

    chore: global style 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { MyStoreProvider } from '@/stores';
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
-import { lightTheme } from '@/theme';
 import { PAGE_URL } from '@/configs';
 import * as Page from '@/pages';
 import * as Compt from '@/components';
+
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from '@/globalStyle';
+import { lightTheme } from '@/theme';
 
 export const App: React.FC = () => {
   const queryClient = new QueryClient();
@@ -26,86 +28,3 @@ export const App: React.FC = () => {
     </QueryClientProvider>
   );
 };
-
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Source+Sans+Pro&display=swap');
-
-  html,
-  #root,
-  body {
-    width: 100%;
-    height: 100%;
-  }
-
-  #root {
-    overflow: hidden;
-  }
-
-  body {
-    margin: 0;
-    font-family: 'Source Sans Pro', sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background-color: ${({ theme }) => theme.bgColor};
-    color: ${({ theme }) => theme.textColor};
-  }
-
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, menu, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  main, menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    vertical-align: baseline;
-  }
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-  input:focus {
-    outline: none;
-  }
-  * {
-    box-sizing: border-box;
-  }
-
-  /* HTML5 display-role reset for older browsers */
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, main, menu, nav, section {
-    display: block;
-  }
-
-  /* HTML5 hidden-attribute fix for newer browsers */
-  *[hidden] {
-      display: none;
-  }
-  body {
-    line-height: 1;
-  }
-  menu, ol, ul {
-    list-style: none;
-  }
-  blockquote, q {
-    quotes: none;
-  }
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-`;
